5a7832a2650484a331954d9f32b37f3b717f43d8
Maximilian <Maximilian_Hedman@hotmail.com>
2022-10-11 10:46:51 +0200

    Lagt in JS

---
+++ app.js
@@ -0,0 +1,38 @@
+//random nummer
+let random = Math.floor(Math.random() * 100 + 1);
+let enter = document.getElementById('enter')
+let inputs = document.querySelectorAll('input')
+let meddela = document.getElementById('spanNumber')
+
+
+enter.addEventListener('click', question)
+
+
+let count = 0;              
+enter.onclick = function () {
+    count++
+}
+
+//funktion
+function question()  { 
+    let input = document.getElementById('insertNumber').value;
+    console.log(random)
+    if (input == random) {
+        console.log('Rätt svar! Antal gissningar: ' + count); 
+        meddela.innerHTML = "" + random
+    }
+    else if (input < 0 || input > 100) {
+        console.log('Antal gissningar: ' + count);
+    }
+    //Om svaret är för lågt
+    else if (input < random) {
+        console.log('Du gissade för lågt! Antal gissningar: ' + count);
+    }
+    //Om svaret är för högt
+    else if (input > random) {
+        console.log('Du gissade för högt! Antal gissningar: ' + count); 
+    }
+    else {
+        console.log('Antal gissningar: ' + count);  
+    }
+};
