dba045d427b2f3923da916a01f8dd5f2652f1e15
Shayes1995 <shayans-s@outlook.com>
2022-10-11 10:41:09 +0200

    ny script

---
+++ app3.js
@@ -0,0 +1,43 @@
+//random nummer
+let random = Math.floor(Math.random() * 100 + 1);
+let enter = document.getElementById('enter')
+let inputs = document.querySelectorAll('input')
+let meddela = document.getElementById('spanNumber')
+
+
+enter.addEventListener('click', question)
+
+
+let count = 0;              
+enter.onclick = function () {
+    count++
+}
+
+//funktion
+function question()  { 
+    let input = document.getElementById('insertNumber').value;
+    console.log(random)
+    if (input == random) {
+        console.log('Rätt svar! Antal gissningar: ' + count); 
+        meddela.innerHTML = "" + random
+    }
+    else if (input < 0 || input > 100) {
+        console.log('Antal gissningar: ' + count);                                                      //laddar funktionen igen eftersom svaret var fel.
+        }
+        //Om svaret är för lågt
+    else if (input < random) {
+        console.log('Du gissade för lågt! Antal gissningar: ' + count);                                                      //laddar funktionen igen eftersom svaret var fel.   
+        }
+        //Om svaret är för högt
+        else if (input > random) {
+            console.log('Du gissade för högt! Antal gissningar: ' + count); 
+            inn                                                     //laddar funktionen igen eftersom svaret var fel.   
+        }
+        else {
+            console.log('Antal gissningar: ' + count);  
+        }
+    
+};
+
+
+console.log('randomtal: ' + random);
