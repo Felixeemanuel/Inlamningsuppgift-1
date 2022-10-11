736eefc32be8e5cf26bdd1a0784f29e2b7c077e9
Shayes1995 <shayans-s@outlook.com>
2022-10-11 12:51:21 +0200

    nyaste

--- app3.js
+++ app3.js
@@ -1,8 +1,9 @@
 //random nummer
 let random = Math.floor(Math.random() * 100 + 1);
 let enter = document.getElementById('enter')
-let inputs = document.querySelectorAll('input')
+let inputs = document.querySelectorAll('input').value;
 let meddela = document.getElementById('spanNumber')
+let correctAnswer = document.getElementById('spanHighLowCorrect')
 
 
 enter.addEventListener('click', question)
@@ -19,22 +20,29 @@ function question()  {
     console.log(random)
     if (input == random) {
         console.log('Rätt svar! Antal gissningar: ' + count); 
-        meddela.innerHTML = "" + random
+        correctAnswer.style.color = '#05ff16'
+        correctAnswer.innerHTML = ('Rätt svar är ') + random 
+        meddela.innerHTML = input
     }
     else if (input < 0 || input < 100) {
-        console.log('Antal gissningar: ' + count);                                                      //laddar funktionen igen eftersom svaret var fel.
+        console.log('Antal gissningar: ' + count);
+        meddela.innerHTML = input                                                   //laddar funktionen igen eftersom svaret var fel.
     }
         //Om svaret är för lågt
     else if (input < random) {
-        console.log('Du gissade för lågt! Antal gissningar: ' + count);                                                      //laddar funktionen igen eftersom svaret var fel.   
+        console.log('Du gissade för lågt! Antal gissningar: ' + count);  
+        meddela.style.color = '#0011ff'
+        meddela.innerHTML = input                                                 //laddar funktionen igen eftersom svaret var fel.   
     }
         //Om svaret är för högt
     else if (input > random) {
-        console.log('Du gissade för högt! Antal gissningar: ' + count); 
+        console.log('Du gissade för högt! Antal gissningar: ' + count);
+        meddela.innerHTML = input
                                                              //laddar funktionen igen eftersom svaret var fel.   
     }
     else {
-        console.log('Antal gissningar: ' + count);  
+        console.log('Antal gissningar: ' + count); 
+        meddela.innerHTML = input
     }
     
 };
