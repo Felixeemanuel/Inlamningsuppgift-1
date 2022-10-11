f69f4b7f1e41a58f4612a0a47ddfa52cb539e733
Maximilian <Maximilian_Hedman@hotmail.com>
2022-10-11 18:40:57 +0200

    hej

---
+++ script.js
@@ -0,0 +1,58 @@
+//random nummer
+let random = Math.floor(Math.random() * 100 + 1);
+let enter = document.getElementById('enter');
+let inputs = document.querySelectorAll('input').value;
+let meddela = document.getElementById('spanNumber');
+let correctAnswer = document.getElementById('spanHighLowCorrect');
+
+
+enter.addEventListener('click', question);
+
+
+let count = 0;              
+enter.onclick = function () {
+    count++
+}
+
+//funktion
+function question()  { 
+    let input = document.getElementById('insertNumber').value;
+    console.log(random)
+    if (input == random) {
+        console.log('Rätt svar! Antal gissningar: ' + count); 
+        correctAnswer.style.color = '#05ff16'
+        correctAnswer.innerHTML = ('Rätt svar är ' ) + random + ('!') + ('<br></br>') + (' Det tog dig ' + count + ' gissningar att svara rätt!');
+        meddela.innerHTML = count;
+    }
+    else if (input < 1 || input > 100) {
+        count--
+        console.log('Du angav ett felaktigt nummer, försök att skriva ett nummer mellan 1-100: (Gissningen räknades inte) ' + count);
+        correctAnswer.style.color = '#c9c9c9'
+        correctAnswer.innerHTML = ('Felaktigt nummer!') + ('<br></br>') + ('Välj ett nummer mellan 1-100');
+        meddela.innerHTML = count;                                                  //laddar funktionen igen eftersom svaret var fel.
+    }
+        //Om svaret är för lågt
+    else if (input < random) {
+        console.log('Du gissade för lågt! Antal gissningar: ' + count); 
+        correctAnswer.style.color = '#0011ff'
+        correctAnswer.innerHTML = ('Du gissade för lågt! Gissa igen.'); 
+        meddela.innerHTML = count;                                                //laddar funktionen igen eftersom svaret var fel.   
+    }
+        //Om svaret är för högt
+    else if (input > random) {
+        console.log('Du gissade för högt! Antal gissningar: ' + count);
+        correctAnswer.style.color = '#ff0000'
+        correctAnswer.innerHTML = ('Du gissade för högt! Gissa igen.');
+        meddela.innerHTML = count;
+    
+    
+        //laddar funktionen igen eftersom svaret var fel.   
+    }
+    else {
+        console.log('Antal gissningar: ' + count); 
+        meddela.innerHTML = count;
+    }
+    
+};
+
+console.log('randomtal: ' + random);
