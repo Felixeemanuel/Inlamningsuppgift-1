9dfefe8c5247985f784eb23f6735032201f56eac
Maximilian <maximilian_hedman@hotmail.com>
2022-10-18 13:40:23 +0200

    Klappar och klart

--- app3.js
+++ app3.js
@@ -1,72 +1,63 @@
-let random = Math.floor(Math.random() * 100 + 1);
+let random = Math.floor(Math.random() * 100 + 1); // Skapar ett random tal mellan 1 - 100
 let enter = document.getElementById('enter');
 let inputs = document.querySelectorAll('input').value;
 let meddela = document.getElementById('spanNumber');
-let correctAnswer = document.getElementById('spanHighLowCorrect');
+let answer = document.getElementById('spanHighLowCorrect');
 const restartBtn = document.getElementById("again")
 const insertNumber = document.getElementById("insertNumber")
 
-//Funktion för att generera ett random nummer
+
 function randomNumber(){
     random = Math.floor(Math.random() * 100 + 1);
 }
 
-enter.addEventListener('click', question);
-
-
-let count = 1;              
+let count = 0;              
 enter.onclick = function () {
     count++
-}
-
-//funktion
-function question()  { 
     let input = document.getElementById('insertNumber').value;
     console.log(random)
     if (input == random) {
         console.log('Rätt svar! Antal gissningar: ' + count); 
-        correctAnswer.style.color = '#05ff16'
-        correctAnswer.innerHTML = ('Rätt svar är ' ) + random + ('!') + ('<br></br>') + (' Det tog dig ' + count + ' gissningar att svara rätt!');
+        answer.style.color = '#05ff16'
+        answer.innerHTML = ('Rätt svar är ' ) + random + ('!') + ('<br></br>') + (' Det tog dig ' + count + ' gissningar att svara rätt!');
         meddela.innerHTML = count;
     }
+        //Om svaret är lärgre än 1 eller högre än 100
     else if (input < 1 || input > 100) {
         count--
         console.log('Du angav ett felaktigt nummer, försök att skriva ett nummer mellan 1-100: (Gissningen räknades inte) ' + count);
-        correctAnswer.style.color = '#c9c9c9'
-        correctAnswer.innerHTML = ('Felaktigt nummer!') + ('<br></br>') + ('Välj ett nummer mellan 1-100');
-        meddela.innerHTML = count;                                                  //laddar funktionen igen eftersom svaret var fel.
+        answer.style.color = '#c9c9c9'
+        answer.innerHTML = ('Felaktigt nummer!') + ('<br></br>') + ('Välj ett nummer mellan 1-100');
+        meddela.innerHTML = count;
     }
         //Om svaret är för lågt
     else if (input < random) {
         console.log('Du gissade för lågt! Antal gissningar: ' + count); 
-        correctAnswer.style.color = '#0011ff'
-        correctAnswer.innerHTML = ('Du gissade för lågt! Gissa igen.'); 
-        meddela.innerHTML = count;                                                //laddar funktionen igen eftersom svaret var fel.   
+        answer.style.color = '#0011ff'
+        answer.innerHTML = ('Du gissade för lågt! Gissa igen.'); 
+        meddela.innerHTML = count;   
     }
         //Om svaret är för högt
     else if (input > random) {
         console.log('Du gissade för högt! Antal gissningar: ' + count);
-        correctAnswer.style.color = '#ff0000'
-        correctAnswer.innerHTML = ('Du gissade för högt! Gissa igen.');
+        answer.style.color = '#ff0000'
+        answer.innerHTML = ('Du gissade för högt! Gissa igen.');
         meddela.innerHTML = count;
-    
-    
-        //laddar funktionen igen eftersom svaret var fel.   
     }
     else {
         console.log('Antal gissningar: ' + count); 
         meddela.innerHTML = count;
     }
-    
-};
+
+}
 
     //Clearar alla meddelande och startar om spelet med ett nytt randomtal
 restartBtn.addEventListener("click", function(){
     meddela.innerHTML = "?"
-    count = 1
-    correctAnswer.innerHTML = "Skriv ett nummer och tryck enter!"
-    correctAnswer.style.color = '#fff'
+    count = 0;
+    answer.innerHTML = "Skriv ett nummer och tryck enter!"
+    answer.style.color = '#fff'
     document.getElementById("insertNumber").value = ""
-    randomNumber()
+    randomNumber();
     console.log(random);
 });
